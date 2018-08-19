d7b8a0a0561b43df0e7aeb926d62cd339412b92b
Daniel Moch <daniel@danielmoch.com>
2018-08-19 07:23:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hook Me Up
+
+A Git hook to automate your Pipenv and Django workflows
+
+# Features
+
+* TODO
---
+++ hookmeup/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""A Git hook to automate your Pipenv and Django workflows"""
+
+__author__ = 'Daniel Moch'
+__email__ = 'daniel@danielmoch.com'
+__version__ = '0.1.0'
+__copyright__ = 'Copyright (c) 2018, Daniel Moch'
